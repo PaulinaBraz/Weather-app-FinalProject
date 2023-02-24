869f8eb19117fbcd14255090af6c14873bd6d3d2
PaulinaBraz <124574292+PaulinaBraz@users.noreply.github.com>
2023-02-24 11:43:42 +0000

    Icon issue fixed

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
